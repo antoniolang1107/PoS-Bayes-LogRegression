d125c6e33916bfa2bc1280fc60d63dbc3ea65409
antoniolang1107 <TonylsLame@users.noreply.github.com>
2023-04-11 21:07:57 -0700

    progress

--- helpers.py
+++ helpers.py
@@ -1,5 +1,56 @@
+from nltk.tokenize import word_tokenize
+import numpy as np
+import pandas as pd
+import os
+
 def load_pos_data(fname):
-    pass
+    file_data = pd.read_csv(fname, sep=" ", header=None)
+    file_data[0] = file_data[0].str.lower()
+    unique_counts = file_data[0].value_counts()
+    filtered = unique_counts.drop(unique_counts[unique_counts==1].index)
+    filtered = filtered.drop(filtered[filtered==2].index)
+    filtered = filtered.drop(filtered[filtered==3].index)
+    filtered = filtered.drop([',','the','.','to','of','a','and','in','\'s','for','that', 'is'])
+    filtered = filtered.to_frame()
+    filtered['tag'] = None
+    filtered['label'] = None
+    # print(filtered)
+    # for row in filtered.iterrows():
+    #     row[1] = 
+    # words_and_labels = filtered.join(file_data)
+    # print(words_and_labels)
+    return file_data
+
 
 def load_spam_data(dirname):
-    pass
+    raw_ham_data = []
+    raw_spam_data = []
+
+    ham_dir = os.path.join(dirname, 'ham')
+    spam_dir = os.path.join(dirname, 'spam')
+    ham_files = os.listdir(os.path.join(dirname, 'ham'))
+    spam_files = os.listdir(os.path.join(dirname, 'spam'))
+
+    ham_emails = []
+    spam_emails = []
+
+    for file in range(5):
+        with open(os.path.join(ham_dir, ham_files[file]), 'r') as file:
+            email = file.read()
+            ham_emails.append((word_to_features(word_tokenize(email)), 1))
+    for file in range(5):
+        with open(os.path.join(spam_dir, spam_files[file]), 'r') as file:
+            email = file.read()
+            spam_emails.append((word_to_features(word_tokenize(email)), 0))
+    combined_emails = ham_emails + spam_emails
+    np.random.shuffle(combined_emails)
+    train = combined_emails[int(len(combined_emails)*0.8):]
+    test = combined_emails[:int(len(combined_emails)*0.8)]
+    return train, test
+
+def word_to_features(line):
+    return dict([word, 1] for word in line)
+
+if __name__ == "__main__":
+    # load_pos_data("pos_test.txt")
+    load_spam_data("enron1")
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,60 @@
 def train_naive_bayes(train_data):
-    pass
+    ham_data, spam_data, train_samples, labels = split_by_label(train_data)
+
+    ham_vocab = get_vocab(ham_data)
+    spam_vocab = get_vocab(spam_data)
+    vocabulary = get_vocab(train_samples)
+    ham_word_probs = {word:0 for word in vocabulary}
+    spam_word_probs = {word:0 for word in vocabulary}
+    # word_count
+    model = {'p_ham':len(ham_data)/len(train_samples),
+             'p_spam':len(spam_data)/len(train_samples),
+             'ham_word_probs':ham_word_probs,
+             'spam_word_probs':spam_word_probs}
+    print(model)
 
 def test_naive_bayes(test_data, model):
-    pass
+    processed_data = process_dataset(test_data)
+    num_correct = 0
+    for document, label in processed_data:
+        # out_label = 'ham' if label else 'spam' #debug
+        # print(f"{document}, {out_label}") #debug
+        doc_p_ham = model['p_ham']
+        doc_p_spam = model['p_prob']
+        for word in document:
+            if word in model['ham_word_probs']:
+                doc_p_ham *= model['ham_word_probs'][word]
+            if word in model['spam_word_probs']:
+                doc_p_spam *= model['spam_word_probs'][word]
+            num_correct += (doc_p_ham > doc_p_spam) == label
+    return num_correct / len(test_data)
+
+def process_dataset(dataset):
+    return dataset
+
+def get_vocab(dataset):
+    vocab = set({})
+    for document in dataset:
+        print(document)
+        for word, _ in document.items():
+            vocab.update([word])
+    return vocab
+
+def split_by_label(dataset):
+    ham_data = []
+    spam_data = []
+    samples = []
+    labels = []
+    for sample in dataset:
+        if sample[1] == 1: ham_data.append(sample[0])
+        if sample[1] == 0: spam_data.append(sample[0])
+        samples.append(sample[0])
+        labels.append(sample[1])
+    return ham_data, spam_data, samples, labels
+
+if __name__ == "__main__":
+    data = [({'hello': 1, 'this':1, 'is':1, 'my':1, 'test':1}, 1),
+            ({'hello': 1, 'welcome':1, 'to':1, 'my':1, 'spam':1}, 0)]
+    model = None
+    train_naive_bayes(data)
+    # test_naive_bayes(data, model)
